378f3972f91656e974640403f606db15022d9e9f
ReallySnow <reallysnow233@gmail.com>
2021-02-19 10:43:14 +0000

    z2_plus:DotOS Bringup
    Change-Id: I78855cb4442e8838f1158e7254df2954dc55186a

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018 The dotOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_z2_plus.mk
+    $(LOCAL_DIR)/dot_z2_plus.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_z2_plus-user \
-    lineage_z2_plus-userdebug \
-    lineage_z2_plus-eng
+    dot_z2_plus-user \
+    dot_z2_plus-userdebug \
+    dot_z2_plus-eng
