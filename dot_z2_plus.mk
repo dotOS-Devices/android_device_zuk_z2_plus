ec92aca6d222c65d128e4b31abd557faa41b7c1a
ReallySnow <reallysnow233@gmail.com>
2021-06-06 17:35:53 +0800

    Revert "z2_plus: Switch fingerprint to zui 4.0"
    Fingerprints using ZUI 4.0 cannot pass CTS, so revert it.
    
    This reverts commit 8460f8b580b000069434d659db67bb80d5b8cd13.

--- dot_z2_plus.mk
+++ dot_z2_plus.mk
@@ -38,9 +38,9 @@ TARGET_VENDOR_DEVICE_NAME := z2_plus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="z2_plus" \
     PRODUCT_NAME="z2_plus" \
-    PRIVATE_BUILD_DESC="z2_plus-user 8.0.0 OPR1.170623.032 4.0.199_181018 release-keys"
+    PRIVATE_BUILD_DESC="z2_plus-user 7.0 NRD90M 3.1.117_171110 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "ZUK/z2_plus/z2_plus:8.0.0/OPR1.170623.032/4.0.199_181018:user/release-keys"
+BUILD_FINGERPRINT := "ZUK/z2_plus/z2_plus:7.0/NRD90M/3.1.117_171110:user/release-keys"
 
 TARGET_VENDOR := zuk
